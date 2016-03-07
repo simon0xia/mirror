3ad1cb17863740c90525f9f4de4233b3bf0ff699
simon0xia <xie.yong_hen@hotmail.com>
2016-03-07 13:50:40 +0800

    新增buff图标。并且skill,buff,icon合到一起成为icon;

--- code/GenerateSettingFile/dat.cpp
+++ code/GenerateSettingFile/dat.cpp
@@ -2,44 +2,6 @@
 #include <QImage>
 
 
-void dat_UI(const QString &outFile, qint32 count)
-{
-	qDebug() << __FUNCTION__ << outFile;
-
-	QFile Wfile(outFile);
-	if (!Wfile.open(QIODevice::WriteOnly))
-	{
-		qDebug() << "open " << outFile << "error.";
-		return;
-	}
-
-	qint32 i = 0;
-	QString strPath;
-	QImage img;
-
-	QDataStream iData(&Wfile);
-	while (i < count)
-	{
-		strPath = QString("./Resources/ui/%1.png").arg(i);
-		if (!QFile::exists(strPath))
-		{
-			strPath = QString("./Resources/ui/0.png");
-		}
-		img = QImage(strPath);
-		if (img.isNull())
-		{
-			qDebug() << "\n****Load Image error:" << strPath;
-			break;
-		}
-		iData << img;
-
-		++i;
-	}
-	Wfile.close();
-
-	qDebug() << __FUNCTION__ << "run over. has" << i << "items";
-}
-
 void dat_item(const QString &outFile, qint32 count)
 {
 	qDebug() << __FUNCTION__ << outFile;
@@ -101,6 +63,44 @@ void dat_monster(const QString &outFile, qint32 count)
 		if (!QFile::exists(strPath))
 		{
 			strPath = QString("./Resources/monster/0.png");
+		}
+		img = QImage(strPath);
+		if (img.isNull())
+		{
+			qDebug() << "\n****Load Image error:" << strPath;
+			break;
+		}
+		iData << img;
+
+		++i;
+	}
+	Wfile.close();
+
+	qDebug() << __FUNCTION__ << "run over. has" << i << "items";
+}
+
+void dat_dat(const QString resPath, const QString &outFile, qint32 count)
+{
+	qDebug() << __FUNCTION__ << outFile;
+
+	QFile Wfile(outFile);
+	if (!Wfile.open(QIODevice::WriteOnly))
+	{
+		qDebug() << "open " << outFile << "error.";
+		return;
+	}
+
+	qint32 i = 0;
+	QString strPath;
+	QImage img;
+
+	QDataStream iData(&Wfile);
+	while (i < count)
+	{
+		strPath = resPath + QString("%1.png").arg(i);
+		if (!QFile::exists(strPath))
+		{
+			strPath = resPath + QString("0.png");
 		}
 		img = QImage(strPath);
 		if (img.isNull())
